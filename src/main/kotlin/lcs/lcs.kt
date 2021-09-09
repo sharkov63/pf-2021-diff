ee1fad2ec6368c547e75e7bbaa2e9aa586c514c5
Данила <duesna897@gmail.com>
2021-09-09 18:47:31 +0300

    Added longest common subsequence (LCS) algorithm

--- src/main/kotlin/lcs/lcs.kt
+++ src/main/kotlin/lcs/lcs.kt
@@ -1,8 +1,64 @@
 package lcs
 
-import trie.*
+/*
+ * Finds any longest common subsequence (LCS) of two sequences a and b
+ * using quadratic dynamic programming approach.
+ *
+ * Returns an array of pairs (i, j),
+ * where first indices form the found subsequence in a,
+ * and second indices form the found subsequence in b.
+ */
+fun <T> longestCommonSubsequence(a: Array<T>, b: Array<T>): Array<Pair<Int, Int>> {
+    // lcsLen[i][j] is the length of the longest common subsequence of prefixes a[0..i-1] and b[0..j-1]
+    val lcsLen = Array(a.size + 1) { Array(b.size + 1) { 0 } }
 
-fun longestCommonSubsequence(a: Array<Trie.Node?>, b: Array<Trie.Node?>): Array<Pair<Int, Int>> {
-    TODO("Make this a template function")
-    TODO("Write LCS")
+    // lcsPrv[i][j] instructs what to do with last elements
+    // of prefixes a[0..i-1] and b[0..j-1] to get the LCS:
+    //   lcsPrv[i][j] == 0   if both elements a[i - 1] and b[j - 1] are taken in the subsequence
+    //   lcsPrv[i][j] == 1   if a[i - 1] is not taken in the subsequence
+    //   lcsPrv[i][j] == 2   if b[j - 1] is not taken in the subsequence
+    val lcsPrv = Array(a.size + 1) { Array(b.size + 1) { -1 } }
+
+    fun recalc(i: Int, j: Int) {
+        if (lcsLen[i - 1][j] >= lcsLen[i][j - 1]) {
+            // throw away a[i - 1]
+            lcsLen[i][j] = lcsLen[i - 1][j]
+            lcsPrv[i][j] = 1
+        } else {
+            // throw away b[j - 1]
+            lcsLen[i][j] = lcsLen[i][j - 1]
+            lcsPrv[i][j] = 2
+        }
+        if (a[i - 1] == b[j - 1] && lcsLen[i - 1][j - 1] + 1 > lcsLen[i][j]) {
+            // take both a[i - 1] and b[j - 1]
+            lcsLen[i][j] = lcsLen[i - 1][j - 1] + 1
+            lcsPrv[i][j] = 0
+        }
+    }
+
+    // Calculating DP
+    for (i in 1..a.size) {
+        for (j in 1..b.size) {
+            recalc(i, j)
+        }
+    }
+
+    var i = a.size
+    var j = b.size
+    var len = lcsLen[i][j]
+    val answer = Array(len) { Pair(-1, -1) }
+    // Restore the LCS backwards using lcsPrv
+    while (len > 0) {
+        when (lcsPrv[i][j]) {
+            0 -> {
+                --i
+                --j
+                answer[--len] = Pair(i, j)
+            }
+            1 -> i--
+            2 -> j--
+            else -> break
+        }
+    }
+    return answer
 }
---
+++ src/test/kotlin/lcsTests.kt
@@ -0,0 +1,58 @@
+import kotlin.test.*
+import lcs.*
+
+internal class lcsTests {
+
+    private fun <T> lcsTestTemplate(a: Array<T>, b: Array<T>, lcsLength: Int) {
+        val lcs = longestCommonSubsequence(a, b)
+        assertEquals(lcs.size, lcsLength)
+        for (t in lcs.indices) {
+            val (i, j) = lcs[t]
+            assertEquals(a[i], b[j])
+            if (t > 0) {
+                assert(lcs[t - 1].first < i)
+                assert(lcs[t - 1].second < j)
+            }
+        }
+    }
+
+    @Test
+    fun lcsTest1() {
+        val a = arrayOf(1, 2, 3, 1, 2, 2, 3, 1, 4)
+        val b = arrayOf(3, 1, 2, 4, 2, 1)
+        val lcsLength = 5
+        lcsTestTemplate(a, b, lcsLength)
+    }
+
+    @Test
+    fun lcsTest2() {
+        val a = arrayOf('A', 'A', 'B', 'A')
+        val b = arrayOf('A', 'A', 'A')
+        val lcsLength = 3
+        lcsTestTemplate(a, b, lcsLength)
+    }
+
+    @Test
+    fun lcsTest3() {
+        val a = arrayOf("aб", "аб", "а", "й", "я", "й", "й", "а", "я")
+        val b = arrayOf("фй", "-+", "12413213123", "й")
+        val lcsLength = 1
+        lcsTestTemplate(a, b, lcsLength)
+    }
+
+    @Test
+    fun lcsTest4() {
+        val a = Array(0) { Pair(0, 0) }
+        val b = arrayOf(Pair(19, 23), Pair(-80, 1337), Pair(11111111, 0))
+        val lcsLength = 0
+        lcsTestTemplate(a, b, lcsLength)
+    }
+
+    @Test
+    fun lcsTest5() {
+        val a = arrayOf(Triple(1, 2, 3), Triple(10, 20, 30), Triple(1, 2, 3), Triple(100, 200, 300))
+        val b = arrayOf(Triple(10, 20, 30), Triple(1, 1,  1), Triple(1, 2, 3), Triple(-1, -1, -10))
+        val lcsLength = 2
+        lcsTestTemplate(a, b, lcsLength)
+    }
+}
