9d10827c5f2456ebc13f6ffa660a5b1d07e1de46
Данила <duesna897@gmail.com>
2021-09-13 11:15:37 +0300

    Removed nullable links in trie implementation. Fixed insert function.

--- src/main/kotlin/trie/trie.kt
+++ src/main/kotlin/trie/trie.kt
@@ -10,7 +10,7 @@ class Trie {
      * Links to children nodes are contained in a MutableMap.
      */
     class Node {
-        var children : MutableMap<Char, Node?> = mutableMapOf()
+        var children: MutableMap<Char, Node> = mutableMapOf()
     }
 
     private val root = Node()
@@ -19,15 +19,11 @@ class Trie {
      * Insert a string into trie.
      * Returns a pointer to the corresponding node in the trie.
      */
-    fun insert(str: String): Node? {
+    fun insert(str: String): Node {
         // We maintain a pointer to current node, and move down
-        var curNode: Node? = root
+        var curNode: Node = root
         for (ch in str) {
-            if (!curNode!!.children.containsKey(ch)) {
-                // There is no downward edge with symbol "ch", so we create one
-                curNode.children[ch] = Node()
-            }
-            curNode = curNode.children[ch]
+            curNode = curNode.children.getOrPut(ch) { Node() }
         }
         return curNode
     }
